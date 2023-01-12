7b7d6bccfa9e2d3ef1277aa13b361673f9c644d3
Dylan Sanders <dms82@pct.edu>
2023-01-12 12:15:58 -0500

    Adding some new features!

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello {
 	public static void main(String[] args) {
-		//System.out.println("whas good g ?");
-		System.out.println("cya");
+		System.out.println("haii!! ^w^");
+		//System.out.println("cya");
+		System.out.println("omg how r u ?? <3");
 	}
 }
---
+++ helloFactory.java
@@ -0,0 +1,7 @@
+public class helloFactory{
+	public string returnHello(int i) {
+		for (int j = 0; j < i; j++) {
+			System.out.println("haii ^w^~");
+		}
+	}
+}
